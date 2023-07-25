dd684f6e8a74d327f99979f5d2dc79627e7206f6
Simona Nikolova <simona.nikolova@vero-digital.solutions>
2023-07-25 15:37:19 +0300

    Remove unneeded permission

--- app/src/main/java/net/kuama/documentscanner/extensions/Context.kt
+++ app/src/main/java/net/kuama/documentscanner/extensions/Context.kt
@@ -4,8 +4,6 @@ import android.content.Context
 import net.kuama.documentscanner.R
 import java.io.File
 
-/** Use external media if it is available, our app's file directory otherwise */
-
 fun Context.outputDirectory() =
     File(filesDir, applicationContext.resources.getString(R.string.app_name)).apply {
         mkdirs()
